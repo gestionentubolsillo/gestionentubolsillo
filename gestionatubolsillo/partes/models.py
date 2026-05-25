da22b2cb3c35b4c91ce664b991b1e55094085a85
albert011 <belatm12@protonmail.com>
2026-05-25 13:19:46 +0200

    refactor-feat:Change partes de trabajo and Add multiples partes

--- gestionatubolsillo/partes/models.py
+++ gestionatubolsillo/partes/models.py
@@ -1,14 +1,73 @@
 from django.db import models
 
 # Create your models here.
-class ParteTrabajo(models.Model):
-    ParteTrabajoID = models.AutoField(primary_key=True)
-    fecha_creacion = models.DateTimeField(auto_now_add=True)
-    #Relacion N:1 con el modelo User
-    #Un parte de trabajo es creado por un unico usuario, pero un usuario puede crear varios partes de trabajo
-    usuario_creador = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='partes_trabajo_creados')
 
-    #Los partes en general contienen un campo de texto con la descripción del parte, pero también admite fotos para documentar el parte
-    descripcion = models.TextField(blank=True, null=True)
-    #TO-DO: Cambiar ruta de subida de fotos
-    foto = models.ImageField(upload_to='partes_trabajo_fotos/', blank=True, null=True)
+class Parte(models.Model):
+    #Clase Abstracta para los diferentes tipos de documentos
+    foto = models.ImageField(upload_to='partes/', blank=True, null=True)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    #Relaciones con otros modelos
+    #1 usuario crea N partes
+    usuario_creador = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='%(class)s_creados')
+    #1 cliente tiene asociado N partes
+    cliente = models.ForeignKey('clientes.Cliente', on_delete=models.CASCADE, related_name='%(class)s_clientes')
+
+
+    fecha_finalizacion = models.DateTimeField(blank=True, null=True)
+    class Meta:
+        abstract = True
+
+
+
+class Parte_Trabajo(Parte):
+    ParteTrabajoID = models.AutoField(primary_key=True)
+    #Campos específicos para partes de trabajo
+    #Relaciones con otros modelos
+    #Usuario creador del parte de trabajo se considera trabajador inicial
+    #0 o 1 usuario releva el parte de trabajo
+    usuario_relevo = models.ForeignKey('users.User', on_delete=models.SET_NULL, blank=True, null=True, related_name='partes_relevo')
+    fecha_hora_relevo = models.DateTimeField(blank=True, null=True)
+    #1 servicio tiene asociado N partes de trabajo
+    servicio = models.ForeignKey('servicios.Servicio', on_delete=models.CASCADE, related_name='partes_servicio')
+    observaciones = models.TextField(blank=True, null=True)
+
+class Linea_Parte_Trabajo(models.Model):
+    LineaParteTrabajoID = models.AutoField(primary_key=True)
+    #Campos específicos para líneas de partes de trabajo
+    #TO-DO: Cambiar el campo de actividad por un campo de eleccion con las actividades predefinidas
+    actividad = models.TextField(blank=True, null=True)
+    extra_info = models.TextField(blank=True, null=True)
+    #Relaciones con otros modelos
+    #1 parte de trabajo tiene asociado N líneas de parte de trabajo
+    parte_trabajo = models.ForeignKey('Parte_Trabajo', on_delete=models.CASCADE, related_name='lineas_parte_trabajo')
+
+class Parte_Incidencia(Parte):
+    ParteIncidenciaID = models.AutoField(primary_key=True)
+    #Campos específicos para partes de incidencia
+    texto_incidencia = models.TextField(blank=True, null=True)
+    fecha_hora_incidencia = models.DateTimeField(auto_now_add=True)
+    #Relaciones con otros modelos
+    #1 usuario asociado a N incidencias
+    usuario_asociado_a_incidencia = models.ForeignKey('users.User',on_delete=models.SET_NULL, blank=True, null=True, related_name='partes_incidencia_sobre_usuario')
+
+class Parte_Inspeccion(Parte):
+    ParteInspeccionID = models.AutoField(primary_key=True)
+    #Campos específicos para partes de inspeccion
+    descripcion = models.TextField(max_length=255)
+    #Relaciones con otros modelos
+    #1 usuario inspector se asocia con N inspecciones
+    inspector_asociado = models.ForeignKey('users.User',on_delete=models.SET_NULL,blank=True,null=True,related_name='partes_inspeccion_inspector_asociado')
+    
+
+class Informe_Acuda(Parte):
+    InformeAcudaID = models.AutoField(primary_key=True)
+    #Campos específicos para informes de acuda
+    descripcion = models.TextField(max_length=255)
+    #Relaciones con otros modelos
+    #1 tecnico inspector registra que acude en N informes de acuda
+    tecnico_asociado = models.ForeignKey('users.User', on_delete=models.PROTECT,related_name='informe_acuda_tecnico_asociado')
+    #1 central receptora recibe N informes de acuda
+    central = models.ForeignKey('centrales.Central', on_delete=models.SET_NULL,null=True,blank=True)
+
+
+
