2466eb2043a1a2a455dbc3e9dc4b21b3474e4ee5
albert011 <belatm12@protonmail.com>
2026-05-27 10:10:46 +0200

    feat: Add CRUD empresas

--- gestionatubolsillo/empresas/models.py
+++ gestionatubolsillo/empresas/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from users.models import User
 # Create your models here.
 
 class Empresa(models.Model):
@@ -14,5 +14,13 @@ class Empresa(models.Model):
     EmpresaID = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=255)
     paquete = models.CharField(max_length=20, choices=PAQUETES_CHOICES, default=None)
+    usuario_creador = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='creador')
+
+
+def can_view_empresas(user:User)-> bool:
+    return user.permisos_empresas == 'view_only' or user.permisos_empresas == 'create_modify'
+
+def can_CRUD_empresas(user:User)->bool:
+    return user.permisos_empresas == 'create_modify'
 
 
