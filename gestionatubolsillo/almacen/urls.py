7b9d7223b15f55bb41c8b79cf7116f9d7e4b85e8
albert011 <belatm12@protonmail.com>
2026-06-05 12:58:46 +0200

    feat: Add CRUD almacen view in html

--- gestionatubolsillo/almacen/urls.py
+++ gestionatubolsillo/almacen/urls.py
@@ -2,4 +2,9 @@ from django.urls import path
 from . import views
 urlpatterns = [
     # Aquí puedes agregar las rutas de tu aplicación
+    path('backoffice/almacen',views.list_almacen),
+    path('backoffice/almacen/create', views.create_almacen_item),
+    path('backoffice/almacen/<int:item_id>',views.almacen_item_details),
+    path('backoffice/almacen/edit/<int:item_id>',views.edit_almacen_item),
+    path('backoffice/almacen/delete/<int:item_id>', views.delete_almacen_item)
 ]
