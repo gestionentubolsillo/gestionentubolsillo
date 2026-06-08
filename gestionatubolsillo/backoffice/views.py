efa85478debf5763d1bbb232419eff8f6bbb34fa
albert011 <belatm12@protonmail.com>
2026-06-08 09:30:18 +0200

    feat: Show Date On Dashboard

--- gestionatubolsillo/backoffice/views.py
+++ gestionatubolsillo/backoffice/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpRequest
 from users.models import User
+from django.utils.timezone import localdate
 
 # Create your views here.
 
@@ -9,6 +10,7 @@ def show_backoffice(request:HttpRequest):
     if not user.is_authenticated:
         return redirect('/login')
     context = {
-        'user':user
+        'user':user,
+        'today': localdate()
     }
     return render(request,'backoffice/home.html',context)
