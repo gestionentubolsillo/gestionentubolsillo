31d07b7e19b80af0a28900c230423dc31cc70f33
albert011 <belatm12@protonmail.com>
2026-06-08 13:26:42 +0200

    feat: Add servicio-cliente asociaciones urls

--- gestionatubolsillo/servicios/urls.py
+++ gestionatubolsillo/servicios/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('backoffice/servicios/create', views.create_servicio),
     path('backoffice/servicios/<int:servicio_id>',views.servicio_details),
     path('backoffice/servicios/edit/<int:servicio_id>',views.edit_servicio),
-    path('backoffice/servicios/delete/<int:servicio_id>', views.delete_servicio)
+    path('backoffice/servicios/delete/<int:servicio_id>', views.delete_servicio),
+    path('backoffice/servicios/<int:servicio_id>/clientes/add',views.add_clientes_to_servicio),
+    path('backoffice/servicios/<int:servicio_id>/clientes/remove',views.remove_clientes_to_servicio)
 ]
