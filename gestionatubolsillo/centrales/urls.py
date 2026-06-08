fd8fde422f0da0a2ff06af92a62528a655560697
albert011 <belatm12@protonmail.com>
2026-06-08 09:30:52 +0200

    feat:Add CRUD of central

--- gestionatubolsillo/centrales/urls.py
+++ gestionatubolsillo/centrales/urls.py
@@ -2,4 +2,9 @@ from django.urls import path
 from . import views
 urlpatterns = [
     # Aquí puedes agregar las rutas de tu aplicación
+    path('backoffice/centrales',views.list_centrales),
+    path('backoffice/centrales/create', views.create_central),
+    path('backoffice/centrales/<int:central_id>',views.central_details),
+    path('backoffice/centrales/edit/<int:central_id>',views.edit_central),
+    path('backoffice/centrales/delete/<int:central_id>', views.delete_central)
 ]
