b273cf74d53fe700405b8f247041c77aba6a80b8
albert011 <belatm12@protonmail.com>
2026-06-16 10:23:11 +0200

    feat: Add relation between user and service

--- gestionatubolsillo/users/urls.py
+++ gestionatubolsillo/users/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('backoffice/users/edit/<int:user_id>',views.edit_user),
     path('backoffice/users/delete/<int:user_id>',views.delete_user),
     path('backoffice/users/permissions/edit/<int:user_id>',views.alter_user_permissions),
-    path('backoffice/users/permissions/<int:user_id>',views.view_user_permissions)
+    path('backoffice/users/permissions/<int:user_id>',views.view_user_permissions),
+    path('backoffice/users/<int:user_id>/services',views.list_services_of_user),
+    path('backoffice/users/<int:user_id>/services/assign',views.assign_services_to_user),
+    path('backoffice/users/<int:user_id>/services/remove',views.remove_services_to_user)
 ]
