ce0eb421222ab403a30118d97c5aadd34cc0af33
albert011 <belatm12@protonmail.com>
2026-06-19 11:13:42 +0200

    feat: Add CRUD to sugerencias -- Needs Refactor

--- gestionatubolsillo/sugerencias/views.py
+++ gestionatubolsillo/sugerencias/views.py
@@ -8,6 +8,7 @@ from django.template import loader
 from django.contrib import messages
 from empresas.models import Empresa
 from django.utils.timezone import now
+from django.db.models.manager import BaseManager
 
 DEFAULT_PAGINATION_SUGERENCIAS = 25
 
@@ -24,18 +25,12 @@ def validate_sugerencia(request:HttpRequest, texto, usuario_referente_id)->bool:
         errors = True
     return errors
 
-#unico servicio de backoffice es el listado de sugerencias y la opcion de cambiar su estado
-@login_required
-@user_passes_test(can_access_backoffice)
-@user_passes_test(can_view_sugerencias)
-def list_sugerencias(request: HttpRequest):
-    #Listado de todas las sugerencias de los usuarios de una empresa
-    #TODO: implementar filtro por departamento
-    #TODO: implementar filtro por estado
-    #TODO: implementar filtro por usuario referente
+def paginate_sugerencias(request:HttpRequest,sugerencias:BaseManager[Sugerencia]):
+    choices = Sugerencia._meta.get_field('estado').choices
     user:User = request.user
-    empresa:Empresa = user.empresa
-    sugerencias = Sugerencia.objects.filter(empresa=empresa).order_by('-fecha_creacion')
+    empresas = Empresa.objects.filter(usuario_creador=user)
+    #TOREFACTOR: Solo seleccionar los usuarios que pertenecen a una misma cuenta
+    users = User.objects.all()
     n_pagina = request.GET.get('page', 1)
     global DEFAULT_PAGINATION_SUGERENCIAS
     n_sugerencias = request.GET.get('n_sugerencias', DEFAULT_PAGINATION_SUGERENCIAS)
@@ -45,60 +40,104 @@ def list_sugerencias(request: HttpRequest):
         'sugerencias': page_obj,
         'page_obj': page_obj,
         'n_pagina': n_pagina,
-        'n_sugerencias': n_sugerencias
+        'n_sugerencias': n_sugerencias,
+        'estados':choices,
+        'empresas':empresas,
+        'usuarios':users
     }
+    return context
+
+def filtra_sugerencias(request:HttpRequest,filter_only_self=False)->tuple[dict,dict]:
+    user:User = request.user
+    empresa:Empresa = user.empresa
+    filtros = {'empresa':empresa}
+    exclusiones = {}
+
+    user_creador_id = request.GET.get('usuario_creador_id')
+    if filter_only_self:
+        filtros['usuario_creador'] = user
+    elif user_creador_id:
+        user = User.objects.filter(UserID=user_creador_id).first()
+        filtros['usuario_creador'] = user
+
+    estado = request.GET.get('estado')
+    if estado:
+        filtros['estado']=estado
+    else:
+        exclusiones['estado']= 'borrada'
+
+    id_empresa = request.GET.get('empresa_id')
+    if id_empresa:
+        empresa = Empresa.objects.filter(EmpresaID=id_empresa).first()
+        filtros['empresa']=empresa
+
+    fecha_inicio = request.GET.get('fecha_inicio')
+    fecha_fin = request.GET.get('fecha_fin')
+    if fecha_inicio:
+        filtros['fecha_creacion__gte'] = fecha_inicio
+    if fecha_fin:
+        filtros['fecha_creacion__lte'] = fecha_fin
+
+    user_ref_id = request.GET.get('user_ref_id')
+    if user_ref_id:
+        user = User.objects.filter(UserID=user_ref_id).first()
+        filtros['usuario_referente']=user
+
+    return filtros, exclusiones
+
+#unico servicio de backoffice es el listado de sugerencias y la opcion de cambiar su estado
+@login_required
+@user_passes_test(can_access_backoffice)
+@user_passes_test(can_view_sugerencias)
+def list_sugerencias(request: HttpRequest):
+
+    filtros, exclusiones=filtra_sugerencias(request)
+    sugerencias = Sugerencia.objects.filter(**filtros).exclude(**exclusiones).order_by('-fecha_creacion')
+    context = paginate_sugerencias(request,sugerencias)
     return render(request,'sugerencias/list.html', context)
 
 #Listado de sugerencias creadas por el usuario, independientemente de si tiene acceso al backoffice
 @login_required
 @user_passes_test(can_view_sugerencias)
 def list_own_sugerencias(request: HttpRequest):
-    #Listado de sugerencias creadas por el usuario logueado
-    #TODO: implementar filtro por departamento
-    #TODO: implementar filtro por estado
-    user:User = request.user
-    sugerencias = Sugerencia.objects.filter(usuario_creador=user).order_by('-fecha_creacion')
-    n_pagina = request.GET.get('page', 1)
-    global DEFAULT_PAGINATION_SUGERENCIAS
-    n_sugerencias = request.GET.get('n_sugerencias', DEFAULT_PAGINATION_SUGERENCIAS)
-    paginator = Paginator(sugerencias, n_sugerencias)
-    page_obj = paginator.get_page(n_pagina)
-    context = {
-        'sugerencias': page_obj,
-        'page_obj': page_obj,
-        'n_pagina': n_pagina,
-        'n_sugerencias': n_sugerencias
-    }
+
+    filtros, exclusiones = filtra_sugerencias(request,filter_only_self=True)
+    sugerencias = Sugerencia.objects.filter(**filtros).exclude(**exclusiones).order_by('-fecha_creacion')
+    context = paginate_sugerencias(request,sugerencias)
     return render(request,'sugerencias/list.html', context)
 
 #No es necesario acceder al backoffice para crear sugerencias
 @login_required
 @user_passes_test(can_CRUD_sugerencias)
 def create_sugerencia(request:HttpRequest):
+    user:User = request.user
+    #TOREFACTOR: añadir clase cuenta y filtrar por usuarios donde user.cuenta = allowed_users.cuenta
+    allowed_users = User.objects.all()
+    template = loader.get_template('sugerencias/form.html')
+    context = {'action':'create','usuarios':allowed_users}
     if request.method == 'POST':
         created_at = now()
         texto = request.POST.get('texto','')
-        departamento = request.POST.get('departamento','Sin departamento')
+        departamento = request.POST.get('departamento')
         usuario_referente_id = request.POST.get('user_ref_id')
         errors = validate_sugerencia(request,texto,usuario_referente_id)
         if errors:
-            template = loader.get_template('form.html')
-            context = {}
             return HttpResponse(template.render(context,request))
+        if departamento == '':
+            departamento = 'Sin Departamento'
         user_ref = User.objects.filter(UserID=usuario_referente_id).first()
         empresa : Empresa = user_ref.empresa
         sugerencia = Sugerencia()
         sugerencia.texto = texto
         sugerencia.departamento = departamento
-        sugerencia.usuario_creador = request.user
+        sugerencia.usuario_creador = user
         sugerencia.usuario_referente = user_ref
         sugerencia.empresa = empresa
         sugerencia.fecha_creacion = created_at
+        sugerencia.estado = 'pendiente'
         sugerencia.save()
-        return redirect('sugerencias/list')
+        return redirect('/backoffice/sugerencias')
     elif request.method == 'GET':
-        template = loader.get_template('form.html')
-        context = {}
         return HttpResponse(template.render(context,request))
 
 
@@ -111,7 +150,7 @@ def update_estado_sugerencia(request:HttpRequest, sugerencia_id):
     sugerencia.estado = estado
     sugerencia.save()
     messages.success(request, 'El estado de la sugerencia ha sido actualizado exitosamente.',extra_tags='success')
-    return redirect('backoffice/sugerencias')
+    return redirect('/backoffice/sugerencias')
 
 @login_required
 @user_passes_test(can_access_backoffice)
@@ -121,7 +160,21 @@ def delete_sugerencia(request:HttpRequest, sugerencia_id):
     sugerencia.estado = 'borrada'
     sugerencia.save()
     messages.success(request, 'La sugerencia ha sido borrada exitosamente.',extra_tags='success')
-    return redirect('backoffice/sugerencias')
+    return redirect('/backoffice/sugerencias')
+
+
+@login_required
+@user_passes_test(can_view_sugerencias)
+def details_sugerencia(request:HttpRequest,sugerencia_id):
+    sugerencia = Sugerencia.objects.filter(SugerenciaID=sugerencia_id).first()
+    if not sugerencia:
+        messages.error(request,"La Sugerencia no existe", extra_tags='error')
+        return redirect('/backoffice/sugerencias')
+    template = loader.get_template('sugerencias/form.html')
+    context = {'action': 'view','sugerencia': sugerencia}
+
+    return HttpResponse(template.render(context,request))
+
 
 
     
