a3c844b815c44e4a7735ae3836f6247b2d74673f
albert011 <belatm12@protonmail.com>
2026-06-19 11:13:42 +0200

    feat: Add CRUD to sugerencias -- Needs Refactor

--- gestionatubolsillo/sugerencias/urls.py
+++ gestionatubolsillo/sugerencias/urls.py
@@ -3,6 +3,9 @@ from . import views
 urlpatterns = [
     # Aquí puedes agregar las rutas de tu aplicación
     path('backoffice/sugerencias',views.list_sugerencias),
-    path('backoffice/sugerencias/self',views.list_own_sugerencias)
+    path('backoffice/sugerencias/self',views.list_own_sugerencias),
+    path('backoffice/sugerencias/create', views.create_sugerencia),
+    path('backoffice/sugerencias/<int:sugerencia_id>/delete',views.delete_sugerencia),
+    path('backoffice/sugerencias/<int:sugerencia_id>',views.details_sugerencia)
 
 ]
