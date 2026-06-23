474699c3b634ac968936594cc1af7b3f8dcf47b0
albert011 <belatm12@protonmail.com>
2026-06-23 09:57:32 +0200

    refactor:refactor centrales

---
+++ gestionatubolsillo/centrales/filters.py
@@ -0,0 +1,8 @@
+from django.http import HttpRequest
+from users.models import User
+
+def filter_centrales(request:HttpRequest)->tuple[dict,dict]:
+    user:User = request.user
+    filtros = {'usuario_creador_id':user.UserID}
+    exclusiones = {}
+    return filtros,exclusiones
