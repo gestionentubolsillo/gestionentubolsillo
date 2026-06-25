1189b7f09eaadc2bd0e6ca5352afa3dadac1d8a2
albert011 <belatm12@protonmail.com>
2026-06-25 11:15:08 +0200

    chore:encripted sensitive data on centrales

--- gestionatubolsillo/centrales/models.py
+++ gestionatubolsillo/centrales/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from users.models import User, tiene_acceso
+from encrypted_fields import EncryptedCharField,EncryptedEmailField
 # Create your models here.
 class Central(models.Model):
     CentralID = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=200)
-    telefono = models.CharField(max_length=20, blank=True)
-    mail = models.EmailField(blank=True)
+    #Datos sensibles
+    telefono = EncryptedCharField(max_length=20,blank=True,null=True)
+    mail = EncryptedEmailField(blank=True,null=True)
     persona_de_contacto = models.CharField(max_length=200, blank=True)
     observaciones = models.TextField(blank=True)
     fecha_creacion = models.DateTimeField(auto_now_add=True)
