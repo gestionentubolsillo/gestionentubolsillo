d9bf67232fce1b77e53ab080794159e63ac4bbe8
albert011 <belatm12@protonmail.com>
2026-06-29 13:11:36 +0200

    refactor: filter servicio shows only user cuenta

--- gestionatubolsillo/servicios/filters.py
+++ gestionatubolsillo/servicios/filters.py
@@ -7,7 +7,7 @@ def filtra_servicios(request:HttpRequest,show_deleted=False)->tuple[dict,dict]:
     user:User = request.user
     empresa:Empresa = user.empresa
     filtro_empresa = request.GET.get('empresa',empresa.EmpresaID)
-    filtros={'empresa_id':filtro_empresa}
+    filtros={'empresa_id':filtro_empresa, 'cuenta_id':user.cuenta.pk}
     #TODO: modificar para no mostrar los borrados
     exclusiones={} if not show_deleted else {}
     return filtros,exclusiones
