e48e61f4625e192e42d12348e27742d76682d1dc
albert011 <belatm12@protonmail.com>
2026-07-07 10:31:59 +0200

    feat: add count of items to dashboard

--- gestionatubolsillo/backoffice/views.py
+++ gestionatubolsillo/backoffice/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import HttpRequest
 from users.models import User
-from django.utils.timezone import localdate
+from django.utils.timezone import localdate, make_aware
+from sugerencias.models import Sugerencia
+from partes.models import Parte_Trabajo, Parte_Incidencia, Informe_Acuda
+from datetime import datetime, time, timedelta
 
 # Create your views here.
 
@@ -9,8 +12,17 @@ def show_backoffice(request:HttpRequest):
     user : User = request.user
     if not user.is_authenticated:
         return redirect('/login')
+    today = localdate()
+    num_sugerencias_today = Sugerencia.objects.filter(cuenta=user.cuenta,fecha_creacion__date=today).count()
+    num_partes_today = Parte_Trabajo.objects.filter(cuenta=user.cuenta,fecha_creacion__date=today).count()
+    num_incidencias_today = Parte_Incidencia.objects.filter(cuenta=user.cuenta,fecha_creacion__date=today).count()
+    num_acudas_today = Informe_Acuda.objects.filter(cuenta=user.cuenta,fecha_creacion__date=today).count()
     context = {
         'user':user,
-        'today': localdate()
+        'today': today,
+        'num_sugerencias':num_sugerencias_today,
+        'num_partes':num_partes_today,
+        'num_incidencias':num_incidencias_today,
+        'num_acudas':num_acudas_today
     }
     return render(request,'backoffice/home.html',context)
