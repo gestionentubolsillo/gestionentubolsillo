5910e3c7895e641235471df6159613ef33f85e5f
albert011 <belatm12@protonmail.com>
2026-07-15 13:18:40 +0200

    add test structure to servicios

--- gestionatubolsillo/servicios/tests.py
+++ gestionatubolsillo/servicios/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from home.tests import BaseTests
+from .models import Servicio
 
 # Create your tests here.
+
+class ServicioTest(BaseTests):
+    def setUp(self):
+        super().setUp()
+
+class ServicioTestsView(ServicioTest):
+    pass
+
+class ServicioTestsCreate(ServicioTest):
+    pass
+
+class ServicioTestsDelete(ServicioTest):
+    pass
+
+class ServicioTestsEdit(ServicioTest):
+    pass
