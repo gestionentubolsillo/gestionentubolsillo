49f5038c2399e27abfb912768598a3b2bf54bb08
albert011 <belatm12@protonmail.com>
2026-07-16 11:24:17 +0200

    refactor: add cuenta to medioaux for auth purposes

--- gestionatubolsillo/mediosaux/tests.py
+++ gestionatubolsillo/mediosaux/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from home.tests import BaseTests
+from .models import MedioAuxiliar
 
 # Create your tests here.
+
+class MedioAuxTest(BaseTests):
+    def setUp(self):
+        super().setUp()
+        medio_auth = MedioAuxiliar(cuenta=self.logged_user.cuenta,
+            nombre='medio__',usuario_creador=self.logged_user)
+        medio_auth.save()
+
+        self.medio_auth = medio_auth
+
+        medio_not_auth = MedioAuxiliar(cuenta=self.other_user.cuenta,
+            nombre='notauth__',usuario_creador=self.other_user)
+        medio_not_auth.save()
+
+        self.medio_not_auth = medio_not_auth
