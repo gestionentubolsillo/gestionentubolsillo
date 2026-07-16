10d56b0d9ea7db4997067d4bd300815e2510068b
albert011 <belatm12@protonmail.com>
2026-07-16 11:25:25 +0200

    refactor: refactor pagination/validation and added migration of cuenta in medioaux

---
+++ gestionatubolsillo/mediosaux/paginators.py
@@ -0,0 +1,20 @@
+from django.http import HttpRequest
+from django.db.models import BaseManager
+from .models import MedioAuxiliar
+from django.core.paginator import Paginator
+
+DEFAULT_PAGINATION_MEDIOS_AUXILIARES = 25
+
+def paginate_medios(request:HttpRequest,lista:BaseManager[MedioAuxiliar])->dict:
+    n_pagina = request.GET.get('page',1)
+    global DEFAULT_PAGINATION_MEDIOS_AUXILIARES
+    n_medios_auxiliares = request.GET.get('n_medios_auxiliares', DEFAULT_PAGINATION_MEDIOS_AUXILIARES)
+    paginacion = Paginator(lista,n_medios_auxiliares)
+    page_obj = paginacion.get_page(n_pagina)
+    context = {
+        'medios_auxiliares': page_obj,
+        'page_obj': page_obj,
+        'page':n_pagina,
+        'n_medios_auxiliares':n_medios_auxiliares
+    }
+    return context
