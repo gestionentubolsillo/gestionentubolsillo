182d4ccc058100464c669e926396bdb3d9f514e2
albert011 <belatm12@protonmail.com>
2026-07-20 10:57:55 +0200

    refactor: change structure to clientes for better readability

--- gestionatubolsillo/clientes/urls.py
+++ gestionatubolsillo/clientes/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from . import views
+from .views import clients,services
 urlpatterns = [
     # Aquí puedes agregar las rutas de tu aplicación
-    path('backoffice/clientes',views.list_clientes),
-    path('backoffice/clientes/create', views.create_client),
-    path('backoffice/clientes/<int:client_id>',views.client_details),
-    path('backoffice/clientes/edit/<int:client_id>',views.edit_client),
-    path('backoffice/clientes/delete/<int:client_id>', views.delete_client),
-    path('backoffice/clientes/<int:client_id>/servicios/add',views.add_servicios_to_cliente),
-    path('backoffice/clientes/<int:client_id>/servicios/remove',views.remove_servicios_to_cliente)
+    path('backoffice/clientes',clients.list_clientes),
+    path('backoffice/clientes/create', clients.create_client),
+    path('backoffice/clientes/<int:client_id>',clients.client_details),
+    path('backoffice/clientes/edit/<int:client_id>',clients.edit_client),
+    path('backoffice/clientes/delete/<int:client_id>', clients.delete_client),
+    path('backoffice/clientes/<int:client_id>/servicios/add',services.add_servicios_to_cliente),
+    path('backoffice/clientes/<int:client_id>/servicios/remove',services.remove_servicios_to_cliente)
 ]
