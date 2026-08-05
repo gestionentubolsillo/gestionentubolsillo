d997a3c1909f738c91d3a0a9866ce23c9f1511f5
albert011 <belatm12@protonmail.com>
2026-08-05 10:00:43 +0200

    chore: add robots.txt to prevent bot indexing

--- gestionatubolsillo/home/urls.py
+++ gestionatubolsillo/home/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     # Aquí puedes agregar las rutas de tu aplicación
     path('', views.home, name='home'),
+    path('robots.txt', views.robots_txt, name='robots_txt'),
 ]
